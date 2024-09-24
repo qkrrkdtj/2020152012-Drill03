d726342b531c9f59cf1f1e3eff3fbf38f1b67467
hyunsuk01@TIP209 <hyunsuk0109@naver.com>
2024-09-24 09:54:28 +0900

    무한루프 확인

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,22 @@
 from pico2d import *
 
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
 open_canvas()
 
 # fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+while True:
+    run_rectangle()
+    run_circle()
+
 
 close_canvas()
