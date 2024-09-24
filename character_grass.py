7800f6890b7e9d0511011b6cd2496cc686b1bb77
hyunsuk01@TIP209 <hyunsuk0109@naver.com>
2024-09-24 10:43:54 +0900

    위쪽 라인 이동 및 서클 이동 구현 완료

--- character_grass.py
+++ character_grass.py
@@ -7,8 +7,15 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x, y):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
+
 def run_top():
     print('TOP')
+    for x in range(0, 800, 10):
+        draw_boy(x,550)
     pass
 
 def run_right():
@@ -42,14 +49,12 @@ def run_circle():
         x = r * math.cos(math.radians(d)) + cx
         y = r * math.sin(math.radians(d)) + cy
         
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.1)
+        draw_boy(x, y)
     
     pass
 
 while True:
-    # run_circle()
+    run_circle()
     run_rectangle()
     break
 
