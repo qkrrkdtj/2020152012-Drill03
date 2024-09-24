3c457cfb101e0b79c64454c86d0cf26c4922fe2e
hyunsuk01@TIP209 <hyunsuk0109@naver.com>
2024-09-24 10:03:17 +0900

    draw character

--- character_grass.py
+++ character_grass.py
@@ -1,22 +1,27 @@
 from pico2d import *
 
-def run_rectangle():
-    print('RECTANGLE')
-    pass
-
-def run_circle():
-    print('CIRCLE')
-    pass
-
 open_canvas()
 
 # fill here
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+def run_circle():
+    print('CIRCLE')
+
+    clear_canvas_now()
+    boy.draw_now(400,0)
+    delay(1)
+    pass
+
 while True:
     run_rectangle()
     run_circle()
+    break
 
 
 close_canvas()
